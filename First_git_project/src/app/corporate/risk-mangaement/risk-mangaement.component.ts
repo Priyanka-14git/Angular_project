64007c0ca260e6409a9ab34183c55f75f64dbd8b
Priyanka-14git <phadatarepm14@gmail.com>
2025-07-01 16:25:16 +0530

    changes related to lazy loading

---
+++ First_git_project/src/app/corporate/risk-mangaement/risk-mangaement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-risk-mangaement',
+  templateUrl: './risk-mangaement.component.html',
+  styleUrls: ['./risk-mangaement.component.css']
+})
+export class RiskMangaementComponent {
+
+}
